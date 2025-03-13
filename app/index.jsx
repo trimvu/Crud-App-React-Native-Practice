9417908955cbfc27b68e7324d1750a0672532088
Tri <vutriminh@gmail.com>
2025-03-13 15:37:24 -0400

    dynamic routing

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter"
 import Animated, { LinearTransition } from 'react-native-reanimated'
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { StatusBar } from 'expo-status-bar'
-
+import { useRouter } from "expo-router";
 import Octicons from '@expo/vector-icons/Octicons'
 
 import { data } from "@/data/todos"
@@ -17,6 +17,8 @@ export default function Index() {
   const [todos, setTodos] = useState([])
   const [text, setText] = useState('')
   const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
+  const router = useRouter()
+
   const [loaded, error] = useFonts({
     Inter_500Medium,
   })
@@ -77,14 +79,23 @@ export default function Index() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const handlePress = (id) => {
+    router.push(`/todos/${id}`)
+  }
+
   const renderItem = ({ item }) => (
     <View style={styles.todoItem}>
-      <Text
-        style={[styles.todoText, item.completed && styles.completedText]}
-        onPress={() => toggleTodo(item.id)}
+      <Pressable
+        onPress={() => handlePress(item.id)}
+        onLongPress={() => toggleTodo(item.id)}
       >
-        {item.title}
-      </Text>
+        <Text
+          style={[styles.todoText, item.completed && styles.completedText]}
+
+        >
+          {item.title}
+        </Text>
+      </Pressable>
       <Pressable onPress={() => removeTodo(item.id)}>
         <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
       </Pressable>
@@ -96,6 +107,7 @@ export default function Index() {
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
+          maxLength={30}
           placeholder="Add a new todo"
           placeholderTextColor="gray"
           value={text}
